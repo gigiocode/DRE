f45e86eb5552ca9212612cc5fe1b8424d0d9903a
gigiocode <giovane.meca@gmail.com>
2024-01-29 18:56:04 -0300

    unhiding firebase ids

--- src/logic/firebase/config/app.ts
+++ src/logic/firebase/config/app.ts
@@ -1,13 +1,13 @@
 import { initializeApp, FirebaseApp } from 'firebase/app'
 
 const app: FirebaseApp = initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-})
+    apiKey: "AIzaSyAJszufscvlVU-UJP-zf6uMrkqRDkRB-sk",
+    authDomain: "meu-dre.firebaseapp.com",
+    projectId: "meu-dre",
+    storageBucket: "meu-dre.appspot.com",
+    messagingSenderId: "949304337387",
+    appId: "1:949304337387:web:93715f5abc76693c1cefae",
+    measurementId: "G-5CF39XS437"
+  })
 
 export { app }
